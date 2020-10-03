bdd4a5a1f989fa34d1b82570c8c590cf3c5fe306
RealLifeCore <72294178+RealLifeCore@users.noreply.github.com>
2020-10-02 23:45:39 -0700

    Add files via upload

---
+++ Assets/Scripts/GameLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameLoader : MonoBehaviour
+{
+   public void LoadGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+}
